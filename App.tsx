2e05b9201293078e599e0821edc9e8012bf897d9
TiagoCiceri <tiagociceri@gmail.com>
2022-02-21 20:50:28 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import UserList from './scr/views/UserList';
+import UserForm from './scr/views/UserForm';
+import { Button, Icon } from 'react-native-elements'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='UserList'
+        screenOptions={screenOptions}>
+
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={({ navigation }) => {
+            return {
+              title: "Lista de Usuário",
+              headerRight: () => (
+                <Button
+                  onPress={() => navigation.navigate("UserForm")}
+                  type="clear"
+                  icon={<Icon name="add" size={25} color="white" />}
+                />
+              )
+            }
+          }}
+        />
+
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{
+            title: "Formulário de Usuário"
+          }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+}
+
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e'
+  },
+  headerTinColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: "bold"
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ scr/views/UserList.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { View, Text, FlatList } from 'react-native'
+import { ListItem } from 'react-native-elements'
+import users from './data/user'
+
+export default props => {
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem
+                leftAvatar={{ source: { uri: user.avatarUrl } }}
+                key={user.id}
+                title={user.name}
+                subtitle={user.email}
+                bottomDivider
+            />
+        )
+
+    }
+
+    return (
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
+    )
+}
