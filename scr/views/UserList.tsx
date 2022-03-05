2c472fc06f43c513c32a7498df945ecd05c2b65e
TiagoCiceri <tiagociceri@gmail.com>
2022-03-05 11:50:37 -0300

    ajuste elements

--- scr/views/UserList.tsx
+++ scr/views/UserList.tsx
@@ -1,21 +1,61 @@
 import React from 'react'
-import { View, Text, FlatList } from 'react-native'
-import { ListItem } from 'react-native-elements'
-import users from './data/user'
+import { View, Text, FlatList, Alert } from 'react-native'
+import { Avatar, ListItem, Button, Icon } from 'react-native-elements'
+import users from './data/users'
 
 export default props => {
 
-    function getUserItem({ item: user }) {
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir usuário', 'Deseja excluir o usuário', [
+            {
+                text: 'Sim',
+                onPress() {
+                    console.warn('Delete '+ user.name)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getActions(user) {
+        return (
+            <>            
+                <Button
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    type="clear"
+                    icon={<Icon name="edit" size={25} color="orange" />}
+                />
+                <Button
+                    onPress={() => confirmUserDeletion(user)}
+                    type="clear"
+                    icon={<Icon name="delete" size={25} color="red" />}
+                />
+
+            </>
+        )
+    }
+
+    function getUserItem({ item }) {
         return (
             <ListItem
-                leftAvatar={{ source: { uri: user.avatarUrl } }}
-                key={user.id}
-                title={user.name}
-                subtitle={user.email}
-                bottomDivider
-            />
+                bottomDivider                
+                onPress={() => props.navigation.navigate('UserForm')}   
+                //rightElement={getActions(item)} deprecate element
+            >
+                <Avatar title={item.name[0]} source={item.avatarUrl && { uri: item.avatarUrl }} />
+                <ListItem.Content>
+                    <ListItem.Title>{item.name}</ListItem.Title>
+                    <ListItem.Subtitle>{item.email}</ListItem.Subtitle>                    
+                </ListItem.Content>
+                
+                <ListItem.Content right >                    
+                    {getActions(item)}
+                </ListItem.Content>
+              
+            </ListItem>
         )
-
     }
 
     return (
